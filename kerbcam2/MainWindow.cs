ee73247c6a47776387e2bb580dd65bfd6ff7fd37
John Beisley <greatred@gmail.com>
2014-12-09 22:24:32 +0000

    Basic main window structure, no content.

--- kerbcam2/MainWindow.cs
+++ kerbcam2/MainWindow.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using KSPPluginFramework;
 using UnityEngine;
 
 namespace kerbcam2 {
-    class MainWindow {
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    class MainWindow : MonoBehaviourWindow {
+        private SimpleStockToolbarButton launcherButton;
+        private WindowResizer resizer;
+
+        #region Lifetime
+        internal override void Awake() {
+            resizer = new WindowResizer(
+                new Rect(20, 100, 600, 500),
+                new Vector2(400, 300));
+            launcherButton = new SimpleStockToolbarButton(
+                delegate() { Visible = true; },
+                delegate() { Visible = false; },
+                ApplicationLauncher.AppScenes.FLIGHT,
+                "icon-36x36.png");
+
+            // MonoBehaviourWindow settings.
+            Visible = false;
+            DragEnabled = true;
+            ClampToScreen = true;
+            TooltipsEnabled = true;
+            WindowCaption = "KerbCam2";
+            WindowOptions = new GUILayoutOption[]{
+                resizer.LayoutMinHeight(),
+                resizer.LayoutMinWidth(),
+            };
+        }
+
+        internal override void OnDestroy() {
+            if (launcherButton != null) {
+                launcherButton.Dispose();
+                launcherButton = null;
+            }
+        }
+        #endregion
+
+        internal override void DrawWindow(int id) {
+            using (GU.Vertical()) {
+                // TODO: Main GUI stuff here. This spacer is a placeholder.
+                GUILayout.FlexibleSpace();
+
+                // Bottom row for window controls.
+                using (GU.Horizontal()) {
+                    GUILayout.FlexibleSpace();
+                    // Handle resizing.
+                    resizer.Position = WindowRect;
+                    resizer.HandleResize();
+                    WindowRect = resizer.Position;
+                }
+            }
+        }
     }
 }
