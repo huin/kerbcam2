629cc838be4e118d2e7652d1ef7966f19fa93193
John Beisley <greatred@gmail.com>
2015-04-06 13:49:41 +0100

    Add + buttons between timekeys.

--- kerbcam2/StoryEditor.cs
+++ kerbcam2/StoryEditor.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 namespace kerbcam2 {
     class StoryEditor {
         private Story story;
+        private static float HEADER_HEIGHT = 40;
+        private static float TIME_WIDTH = 60;
+        private static float FIRSTCOL_WIDTH = 70;
+        private static float ADD_WIDTH = 20;
 
         public StoryEditor(Story story) {
             this.story = story;
@@ -12,28 +16,34 @@ namespace kerbcam2 {
         public void DrawUI() {
             // Render the table.
             using (GU.Vertical()) {
-                // Column headers.
+                GUILayoutOption headerHeight = GUILayout.Height(HEADER_HEIGHT);
+                GUILayoutOption addWidth = GUILayout.Width(ADD_WIDTH);
+                GUILayoutOption timeWidth = GUILayout.Width(TIME_WIDTH);
+                GUILayoutOption firstColWidth = GUILayout.Width(FIRSTCOL_WIDTH);
+                // Column headers from timeline.
                 using (GU.Horizontal()) {
-                    GUILayout.Label("Operation", Styles.marginlessLabel,
-                            GUILayout.Height(40), GUILayout.Width(70));
+                    GUILayout.Label("Operation", Styles.marginlessLabel, headerHeight, firstColWidth);
+                    GUILayout.Button("+", Styles.marginlessButton, headerHeight, addWidth);
                     foreach (TimeKey time in story.Timeline) {
                         string timekeyLabel = string.Format("{0}\n{1}",
                                 time.GetTimeFormatted(),
                                 time.name);
-                        GUILayout.Button(timekeyLabel, Styles.marginlessButton,
-                            GUILayout.Height(40), GUILayout.Width(60));
+                        GUILayout.Button(timekeyLabel, Styles.marginlessButton, headerHeight, timeWidth);
+                        GUILayout.Button("+", Styles.marginlessButton, headerHeight, addWidth);
                     }
                 }
-                // Example rows.
+                // Operation rows.
                 foreach (IOperation op in story.EnumerateOperations()) {
                     using (GU.Horizontal()) {
-                        GUILayout.Button(op.Name, Styles.marginlessButton, GUILayout.Width(70));
+                        GUILayout.Button(op.Name, Styles.marginlessButton, firstColWidth);
+                        GUILayout.Space(ADD_WIDTH);
                         foreach (TimeKey time in story.Timeline) {
                             string description;
                             if (op.GetNameForTimeKey(time.id, out description)) {
-                                GUILayout.Button(description, Styles.marginlessButton, GUILayout.Width(60));
+                                GUILayout.Button(description, Styles.marginlessButton, timeWidth);
+                                GUILayout.Space(ADD_WIDTH);
                             } else {
-                                GUILayout.Space(60);
+                                GUILayout.Space(TIME_WIDTH+ADD_WIDTH);
                             }
                         }
                     }
